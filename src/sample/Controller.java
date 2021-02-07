05e70a3bfdf32c0af9f411453f638c78a3d0fa5b
italianot <radioctiveboi@mail.ru>
2021-02-07 21:23:19 +0800

    improvements

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,10 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
@@ -31,28 +29,11 @@ public class Controller implements Initializable{
     @FXML
     public Slider sldH;
     @FXML
-    public Slider sldR;
-    @FXML
     public Slider sldZoom;
-
-    @FXML
-    public TextField upperBase;
-
-    @FXML
-    public TextField lowerBase;
-
-    @FXML
-    public TextField height;
-
     @FXML
     public Label messageLabel;
-
     @FXML
     public Label finalLabel;
-
-    @FXML
-    public Button button;
-
     @FXML
     public Canvas mainCanvas;
 
@@ -63,21 +44,23 @@ public class Controller implements Initializable{
     public void initialize(URL location, ResourceBundle resources) {
 
         draw();
+        count();
 
         sldUp.valueProperty().addListener((observable, oldValue, newValue) -> {
             draw();
+            count();
         });
         sldLow.valueProperty().addListener((observable, oldValue, newValue) -> {
             draw();
+            count();
         });
         sldH.valueProperty().addListener((observable, oldValue, newValue) -> {
             draw();
-        });
-        sldR.valueProperty().addListener((observable, oldValue, newValue) -> {
-            draw();
+            count();
         });
         sldZoom.valueProperty().addListener((observable, oldValue, newValue) -> {
             draw();
+            count();
         });
 
     }
@@ -106,39 +89,78 @@ public class Controller implements Initializable{
             double up = sldUp.getValue();
             double low = sldLow.getValue();
             double h = sldH.getValue();
-            double r = sldR.getValue();
 
+            double c = (low - up) / 2;
+            double a = Math.sqrt(Math.pow(h, 2) + Math.pow(c, 2));
 
+            double r = (c + h - a) / 2;
+            double d = 2 * r;
+
+            //толщина линии
             ctx.setLineWidth(2. / zoom);
-
-            ctx.strokeLine(
-                    -up / 2,
-                    h / 2,
-                    -up / 2,
-                    -h / 2
+            //треугольник
+            ctx.strokePolygon(
+                    new double[]{-up / 2, -up / 2, -low / 2},
+                    new double[]{h / 2, -h / 2, -h / 2 },
+                    3
             );
+            //заливка треугольника
+            ctx.setFill(Color.RED);
+            ctx.fillPolygon(
+                    new double[]{-up / 2, -up / 2, -low / 2},
+                    new double[]{h / 2, -h / 2, -h / 2 },
+                    3
+            );
+            //трапеция
             ctx.strokePolygon(
                     new double[]{-up / 2, up / 2, low / 2, -low / 2},
                     new double[]{h / 2, h / 2, -h / 2, -h / 2 },
                     4
             );
 
-
-
-            /*ctx.strokeArc(
-                     low / 2 - r,
-                    -h / 2 - r,
+            //окружность
+            ctx.strokeOval(//криво работает
+                    (-up / 2) - d,
+                    (-h / 2) + d,
                     2 * r,
+                    2 * r
+            );
+            ctx.setFill(Color.WHITE);
+            ctx.fillOval(//криво работает
+                    (-up / 2) - d,
+                    (-h / 2) + d,
                     2 * r,
-                    -90,
-                    -90,
-                    ArcType.ROUND
-            );*/
+                    2 * r
+            );
 
             ctx.restore();// восст. состояние матрицы
+        }
 
+        void count(){
+            double up = sldUp.getValue();
+            double low = sldLow.getValue();
+            double h = sldH.getValue();
 
+            if (up < low){
+                /*
+                       /|
+                    a / | b
+                     /  |
+                    /___|
+                      c
+                */
 
+                double c = (low - up) / 2;
+                double a = Math.sqrt(Math.pow(h, 2) + Math.pow(c, 2));
+                double p = (a + h + c) / 2;// полупериметр треугольника
+                double s = ((c * h) / 2) - (Math.PI * Math.pow((p - a),2)) ;// площадь треугольника минус площадь окружности
+
+                messageLabel.setText("Площадь посчитана. Фигура нарисована.");
+                finalLabel.setText( "Площадь равна: " + s);
+            }
+            else {
+                messageLabel.setText("Верхнее основание должно быть меньше нижнего!");
+            }
         }
 
     public void onMouseDragged(MouseEvent mouseEvent) {
@@ -155,55 +177,4 @@ public class Controller implements Initializable{
         pressedX = mouseEvent.getX();
         pressedY = mouseEvent.getY();
     }
-
-
-        /*public void buttonClick(ActionEvent actionEvent) {
-            double up = Integer.parseInt(upperBase.getText());// верхнее основание
-            double low = Integer.parseInt(lowerBase.getText());// нижнее основание
-            double b = Integer.parseInt(height.getText());// высота
-
-
-
-            /*Double Up = up;
-            Double Low = low;
-            Double B = b;
-
-            if (Up != null && Low != null && B != null){
-            // здесь должен быть код который ниже
-                messageLabel.setText("Площадь посчитана. Фигура нарисована.");
-            }else{
-                messageLabel.setText("Ведите данные!");
-            }*/
-
-            //if (up < low){
-                /*
-                       /|
-                    a / | b
-                     /  |
-                    /___|
-                      c
-                */
-
-                /*double c = (low - up) / 2;
-                double a = Math.sqrt(Math.pow(b, 2) + Math.pow(c, 2));
-                double p = (a + b + c) / 2;// полупериметр треугольника
-                double s = ((c * b) / 2) - (Math.PI * Math.pow((p - a),2)) ;// площадь треугольника минус площадь окружности
-
-                draw();
-
-
-                messageLabel.setText("Площадь посчитана. Фигура (пока не)нарисована.");
-                finalLabel.setText( "Площадь равна: " + s);
-            }
-            else {
-                messageLabel.setText("Верхнее основание должно быть меньше нижнего!");
-            }
-        }*/
-
-
-
-
 }
-
-
-
